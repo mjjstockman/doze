52b02fc4c73e31d4f8020378729ae03e5195cb7f
Tony K. <tony-koke@outlook.com>
2024-02-08 14:53:07 +0000

    D-2 Basic Dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,18 +1,13 @@
-import HeaderContainer from "../components/DashboardComponents/HeaderContainer.js/HeaderContainer";
-import MainGoalsContainer from "../components/DashboardComponents/MainGoalsContainer/MainGoalsContainer";
-import SubGoalsContainer from "../components/DashboardComponents/SubGoalsContainer/SubGoalsContainer";
+import HeaderContainer from "../../components/DashboardComponents/HeaderContainer/HeaderContainer";
+import MainGoalsContainer from "../../components/DashboardComponents/MainGoalsContainer/MainGoalsContainer";
+import SubGoalsContainer from "../../components/DashboardComponents/SubGoalsContainer/SubGoalsContainer";
 
 const DashboardPage = () => {
-  const TempNav = () => <div className="h-20 w-screen bg-neutral-200 border-b border-b-2 bg-[#DFF3E4]"></div>;
-
   return (
-    <div className="h-min-screen w-screen bg-white flex flex-col overscroll-none">
-      <TempNav />
-      <div className="px-40 py-6 h-full">
-        <HeaderContainer />
-        <MainGoalsContainer />
-        <SubGoalsContainer />
-      </div>
+    <div className="h-min-screen w-screen bg-white flex flex-col overscroll-none px-40 py-6 h-full">
+      <HeaderContainer />
+      <MainGoalsContainer />
+      <SubGoalsContainer />
     </div>
   );
 };
